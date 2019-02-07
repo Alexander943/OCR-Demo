6639f89e95b735c0679da0acdfefd8a212899470
Alexander <alex93anokhin@gmail.com>
2019-02-07 19:31:59 +0300

    MainViewModel class added.

--- app/src/main/java/com/mlkit/textrecognition/app/di/components/ViewModelModule.kt
+++ app/src/main/java/com/mlkit/textrecognition/app/di/components/ViewModelModule.kt
@@ -1,10 +1,13 @@
 package com.mlkit.textrecognition.app.di.components
 
-import com.mlkit.textrecognition.presentation.utils.AppViewModelFactory
-
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.mlkit.textrecognition.app.di.annotation.ViewModelKey
+import com.mlkit.textrecognition.presentation.modules.main.MainViewModel
+import com.mlkit.textrecognition.presentation.utils.AppViewModelFactory
 import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
@@ -12,8 +15,8 @@ abstract class ViewModelModule {
     @Binds
     internal abstract fun bindViewModelFactory(factory: AppViewModelFactory): ViewModelProvider.Factory
 
-    //    @Binds
-    //    @IntoMap
-    //    @ViewModelKey(SplashViewModel.class)
-    //    abstract ViewModel bindSplashViewModel(SplashViewModel splashViewModel);
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    internal abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
 }
--- app/src/main/java/com/mlkit/textrecognition/presentation/modules/main/MainActivity.kt
+++ app/src/main/java/com/mlkit/textrecognition/presentation/modules/main/MainActivity.kt
@@ -2,19 +2,29 @@ package com.mlkit.textrecognition.presentation.modules.main
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.mlkit.textrecognition.R
 import com.mlkit.textrecognition.presentation.base.BaseActivity
+import com.mlkit.textrecognition.presentation.utils.AppViewModelFactory
 import kotlinx.android.synthetic.main.main_activity.*
 import timber.log.Timber
+import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
+
+    @Inject
+    lateinit var viewModelFactory: AppViewModelFactory
+
+    private lateinit var viewModel: MainViewModel
 
     override val layoutResource = R.layout.main_activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this@MainActivity, viewModelFactory).get(MainViewModel::class.java)
 
         ma_btn_bitmap.setOnClickListener { performBitmapRecognition() }
     }
